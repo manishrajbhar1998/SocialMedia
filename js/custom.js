7133836c5c035c3f0448cb94edba7b219c083ac3
manishrajbhar1998 <manishrajbhar379@gmail.com>
2023-08-06 00:43:52 +0530

    owl added

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,49 @@
 // Header Scroll
 
 let nav = document.querySelector('.navbar');
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 20){
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
         nav.classList.add('header_scrolled');
-    }else{
-        nav.classList.remove('header_scrolled'); 
+    } else {
+        nav.classList.remove('header_scrolled');
     }
 }
 
 // nav hide
 let navbar = document.querySelectorAll('.nav-link');
 let navCollapse = document.querySelector('.navbar-collapse.collapse');
-navbar.forEach(function(a){
-    a.addEventListener('click',function(){
+navbar.forEach(function (a) {
+    a.addEventListener('click', function () {
         navCollapse.classList.remove('show');
     })
 })
 
 
 //  Owl Carousel
-$(document).ready(function(){
-    $(".client_slider_section").owlCarousel({
-        items:4,
-        loop:true,
-        nav:false,
-        autoplay:true,
-        autoplayTimeout:1000,
-        autoplayHoverPause:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:2
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        margin: 20,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:6
+            1000: {
+                items: 3
             }
         }
     })
 });
+
+
+
+
